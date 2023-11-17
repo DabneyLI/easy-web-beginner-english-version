547eff1e40d8d17196027ba1383f20fb583dd729
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-11-17 16:45:40 +0100

    Language Selector Updating

--- LanguageSwitcher.js
+++ LanguageSwitcher.js
@@ -1,15 +1,21 @@
 document.getElementById('languageSelector').addEventListener('change', function () {
-  var language = this.value;
-  switch (language) {
-    case 'en':
-        window.location.href = 'https://en.einfachwebsiteerstellen.de/'; // 设置为根目录下的 en 文件夹中的 index_en.html
-        break;
-    case 'de':
-        window.location.href = 'https://einfachwebsiteerstellen.de/'; // 替换为德语页面的实际 URL
-        break;
-    case 'cn':
-        window.location.href = 'https://cn.einfachwebsiteerstellen.de/'; // 替换为中文页面的实际 URL
-        break;
-    // 可以根据需要添加更多的 case
-}
+    var language = this.value;
+    var currentUrl = new URL(window.location.href);
+
+    // 获取当前页面的路径，例如 "/KI-Website-Generator/index.html"
+    var currentPath = currentUrl.pathname;
+
+    // 根据选择的语言构建新的子域名
+    var newSubdomain = language === 'de' ? '' : language + '.';
+    var newUrl;
+
+    // 创建一个新的URL对象，用新的子域名和当前路径
+    if (language === 'de') {
+        newUrl = new URL('https://' + currentUrl.hostname + currentPath);
+    } else {
+        newUrl = new URL('https://' + newSubdomain + 'einfachwebsiteerstellen.de' + currentPath);
+    }
+
+    // 跳转到新的URL
+    window.location.href = newUrl.href;
 });
